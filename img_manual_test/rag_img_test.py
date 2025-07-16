da9e2e0316fcae5e93a3da8b2b18b12f74474fce
rwr9857 <rwr9857@gmail.com>
2025-07-16 15:35:55 +0900

    Chore: 이미지 조회 테스트

--- img_manual_test/rag_img_test.py
+++ img_manual_test/rag_img_test.py
@@ -5,18 +5,22 @@ from utils.index import image_to_base64
 def main():
     """메인 실행 함수"""
 
+    VECTOR_DB_DIR = "./chroma"
+    COLLECTION_NAME= "imgs"
+    EMBEDDINGS_MODEL = "text-embedding-3-small"
+
     # 설정 생성
     config = IndexConfig(
-        persistent_directory="./chroma",
-        collection_name="imgs",
-        embedding_model="text-embedding-3-small",
+        persistent_directory=VECTOR_DB_DIR,
+        collection_name=COLLECTION_NAME,
+        embedding_model=EMBEDDINGS_MODEL,
     )
 
     # 인덱서 생성 및 실행
     indexer = RAGIndexer(config)
 
     # 이미지 로드해서 모델명 검색
-    img = "./data/samsung_imgs/아가사랑_3kg_WA30DG2120EE.png"
+    img = "./data/imgs/samsung/아가사랑_3kg_WA30DG2120EE/그레이지/아가사랑_3kg_WA30DG2120EE_그레이지_0001.png"
     img_base64 = image_to_base64(img)
 
     # 유사도 검색
